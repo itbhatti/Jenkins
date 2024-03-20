eb21032f4a7b6541daa3b8bf887425d0f589a068
itbhatti <110391456+itbhatti@users.noreply.github.com>
2024-03-20 15:46:22 +0530

    Update HelloWorld.java
    Years code floating

--- hello-world/HelloWorld.java
+++ hello-world/HelloWorld.java
@@ -1,3 +1,16 @@
-public class HelloWorld {
-  public static void main(string[] args) {system.out.printIn("Hello, World");}
-}
+year = int(input("year "))
+  m = int(input("month range (1-12): "))
+  if m > 12:
+    print ("retry")
+  else:
+  if m == 2:
+    if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
+      d = 29
+    else:
+      d = 28
+  elif m in [4,6,9,11]:
+      d = 28
+  else:
+      d = 31
+  print (No. of days :", d)
+  
